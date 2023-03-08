2350d7430ae8ddbbdf1b25972bab839ca1c1b279
JOSEPH <lena.boya26@gmail.com>
2023-03-08 16:18:14 +0100

    hh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
+// import { useInView } from "react-intersection-observer";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
