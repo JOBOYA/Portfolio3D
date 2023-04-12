7a0b1c0be0b814574e8ed59049d95db959f272e9
JOSEPH <lena.boya26@gmail.com>
2023-04-12 19:11:35 +0200

    chunks

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,14 @@ export default {
       limit: 10, // Ajustez cette limite en fonction de vos besoins
       include: ['**/*.png', '**/*.jpg', '**/*.gif', '**/*.svg'], // Ajoutez d'autres formats d'image si nécessaire
     }),
-    // ...
-  ],
+     
+      
+    ],
+    output: {
+        // ...
+        manualChunks: {
+          // Crée un morceau séparé pour les bibliothèques tierces
+          'vendor': ['react', 'react-dom', 'lodash']
+        }
+      }
 };
