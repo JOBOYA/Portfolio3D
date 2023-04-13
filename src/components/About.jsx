50e786cd2e805dabb212200443dc116520a5b585
JOSEPH <lena.boya26@gmail.com>
2023-04-13 08:33:46 +0200

    depend

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import * as Tilt from "react-tilt";
-
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-// import { useInView } from "react-intersection-observer";
+
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
